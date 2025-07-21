cd91d1d0e7d1dbe3a081727f58638569809a3fc8
ReenataMay <reenvma@gmail.com>
2025-07-21 17:54:21 +0700

    Create Praktikum_SEARCH.py

---
+++ Praktikum_SEARCH.py
@@ -0,0 +1,14 @@
+import sqlite3
+
+connect_to_db = sqlite3.connect("Cars.db")
+
+k = connect_to_db.cursor()
+
+k.execute("""
+        SELECT * FROM TBCars
+        WHERE
+          id = 101
+        """)
+
+print(k.fetchall())
+connect_to_db.close()
